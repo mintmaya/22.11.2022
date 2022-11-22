9853dc560e89451ab85a2dfbc090ff043bab44c5
mintmaya <118422299+mintmaya@users.noreply.github.com>
2022-11-22 14:08:02 +0900

    Initial commit

---
+++ 22.11.2022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+        }
+    }
+}
